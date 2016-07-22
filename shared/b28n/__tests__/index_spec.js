fcfa11d62744ffb018b9846eba2fd70e8924c763
xiechengyu <taijisanfeng@yahoo.com>
2016-07-22 10:10:10 +0800

    web: 修正默认语言显示错误问题

--- shared/b28n/__tests__/index_spec.js
+++ shared/b28n/__tests__/index_spec.js
@@ -1,28 +1,31 @@
-import {expect, assert} from 'chai';
-
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
 import b28n from '../index';
 
 describe('b28n', () => {
-
   it('version not empty', () => {
-    expect(b28n.version).to.not.empty;
+    expect(b28n.version !== '').to.be.equal(true);
   });
 
   describe('#getLang() and #setLang()', () => {
+    it('should return default lang "en" when init with empty or not support lang', () => {
+      b28n.setLang('');
+
+      expect(b28n.getLang()).to.be.equal('en');
+    });
 
     it('should return default lang "en" when init with empty or not support lang', () => {
       b28n.setLang('');
       expect(b28n.getLang()).to.be.equal('en');
 
       b28n.init({
-        lang: 'hr'
+        lang: 'hr',
       });
 
       expect(b28n.getLang()).to.be.equal('en');
     });
 
     it('should return default lang "en" when setLang with empty or not support lang', () => {
-
       b28n.setLang('');
       expect(b28n.getLang()).to.be.equal('en');
       b28n.setLang('ok');
@@ -30,38 +33,33 @@ describe('b28n', () => {
     });
 
     it('should return lang when setLang with support lang', () => {
-
       b28n.setLang('cn');
 
       expect(b28n.getLang()).to.be.equal('cn');
     });
-
   });
 
   describe('#addDict()', () => {
     it('should return added dict', () => {
-      b28n.addDict({adb: 'dasdds'}, 'cn');
+      b28n.addDict({ adb: 'dasdds' }, 'cn');
       expect(b28n.getDict('cn').adb).to.be.equal('dasdds');
-      b28n.addDict({adb: 'dasdds'});
+      b28n.addDict({ adb: 'dasdds' });
       expect(b28n.getDict().adb).to.be.equal('dasdds');
     });
-
   });
 
   describe('#translate()', () => {
-    let testDict = {
-      "english": "英语",
-      "dict": "词典"
+    const testDict = {
+      english: '英语',
+      dict: '词典',
     };
 
     b28n.addDict(testDict, 'cn');
     it('should do nothing when key not in dict', () => {
-
-      expect(b28n.translate('nothing')).to.be.equal('nothing')
+      expect(b28n.translate('nothing')).to.be.equal('nothing');
     });
 
     it('should translate with added dict', () => {
-
       expect(b28n.translate('english')).to.be.equal('英语');
       b28n.setLang('en');
       expect(b28n.translate('english')).to.be.equal('english');
@@ -69,16 +67,16 @@ describe('b28n', () => {
   });
 
   describe('#_()', () => {
-    let testDict = {
-      "english": "英语",
-      "dict": "词典",
-      "This is %s and %s": "这是 %s 和 %s"
+    const testDict = {
+      english: '英语',
+      dict: '词典',
+      'This is %s and %s': '这是 %s 和 %s',
     };
     const _ = window._;
     b28n.addDict(testDict, 'cn');
 
     it('should do nothing when key not in dict', () => {
-      expect(_('nothing')).to.be.equal('nothing')
+      expect(_('nothing')).to.be.equal('nothing');
     });
 
     it('should translate with added dict', () => {
@@ -93,5 +91,4 @@ describe('b28n', () => {
       expect(_('This is %s and %s', 12, 23)).to.be.equal('这是 12 和 23');
     });
   });
-
 });
--- shared/b28n/index.js
+++ shared/b28n/index.js
@@ -2,36 +2,65 @@
 //  Released under the MIT License
 //	versions 3.0.0
 //	$Id: b28new.js 2014-12-23 ETw $
-/***********************************************************************************************
+/** *********************************************************************************************
  ************************************************************************************************/
 import storage from '../utils/lib/storage';
 import strUtils from '../utils/lib/string';
 import utilsCore from '../utils/lib/core';
 
-const b28n = (function(doc, win) {
+const b28n = (function _b28n(doc, _win) {
   const STROE_KEY = 'B_LANGUAGE';
   const DEFAULT_OPTIONS = {
-    supportLang: ["en", "cn"],
-    defaultLang: 'en'
+    supportLang: ['en', 'cn'],
+    defaultLang: 'en',
   };
   const special = {
-    "zh": "cn",
-    "zh-chs": "cn",
-    "zh-cn": "cn",
-    "zh-cht": "cn",
-    "zh-hk": "zh",
-    "zh-mo": "zh",
-    "zh-tw": "zh",
-    "zh-sg": "zh"
+    zh: 'cn',
+    'zh-chs': 'cn',
+    'zh-cn': 'cn',
+    'zh-cht': 'cn',
+    'zh-hk': 'zh',
+    'zh-mo': 'zh',
+    'zh-tw': 'zh',
+    'zh-sg': 'zh',
   };
-  const b28n = {};
-  
-  // Local Config
+  const win = _win;
+  const dicts = {};
+  const localB28n = {};
+
+  // Local can change Config
   let localLang = 'en';
   let localOptions = DEFAULT_OPTIONS;
-  let dicts = {};
   let currDict;
- 
+
+  /**
+   * find support lang in supports array
+   */
+  function isSupportLang(lang, supports) {
+    const len = supports.length;
+    let i;
+
+    for (i = 0; i < len; i++) {
+      if (lang === supports[i]) {
+        return supports[i];
+      }
+    }
+
+    return false;
+  }
+
+  function getLangWithBrowserSetting() {
+    let ret = (win.navigator.language || win.navigator.userLanguage ||
+        win.navigator.browserLanguage || win.navigator.systemLanguage ||
+        localLang).toLowerCase();
+
+    ret = special[ret] || ret.split('-')[0].toString();
+
+    ret = isSupportLang(ret, DEFAULT_OPTIONS) || 'en';
+
+    return ret;
+  }
+
   function toObject(val) {
     if (val === null || val === undefined) {
       throw new TypeError('b28n.addDict cannot be called with null or undefined');
@@ -39,184 +68,171 @@ const b28n = (function(doc, win) {
 
     return Object(val);
   }
-  
+
   function saveLangWithBrowserLocal(lang) {
     storage.set(STROE_KEY, lang);
   }
 
   function getLangWithBrowserLocal() {
-    var ret = storage.get(STROE_KEY);
-  
+    const ret = storage.get(STROE_KEY);
+
     return ret;
   }
 
-  /**
-   * find support lang in supports array
-   */
-  function isSupportLang(lang, supports) {
-    var len = supports.length;
-    var i;
-    
-    for (i = 0; i < len; i++) {
-      if (lang === supports[i]) {
-        return supports[i];
-      }
-    }
-  }
+  function extend(target, ...rest) {
+    let ret = null;
 
-  b28n.extend = function (target, source) {
-    var len = arguments.length;
-    var ret;
-
-    if(len === 1) {
-      utilsCore.objectAssign(this, target)
+    if (rest.length < 1) {
+      utilsCore.objectAssign(this, target);
       return this;
     }
 
-    ret = utilsCore.objectAssign.apply(Object, [].slice.call(arguments))
+    ret = utilsCore.objectAssign.call(Object, target, ...rest);
 
     return ret;
-  };
+  }
 
-  b28n.extend({
+  localLang = getLangWithBrowserSetting();
+
+  localB28n.extend = extend;
+  localB28n.extend({
     version: '0.0.1',
 
-    isSupport(lang, supports) {    
-      return isSupportLang(lang, localOptions.supportLang)
+    isSupport(lang) {
+      return isSupportLang(lang, localOptions.supportLang);
     },
-    
-    addDict(dict, lang) {
-      lang = this.isSupport(lang) || this.getLang();
-      dict = toObject(dict);
-      
+
+    addDict(_dict, _lang) {
+      const lang = this.isSupport(_lang) || this.getLang();
+      const dict = toObject(_dict);
+
       if (dicts[lang]) {
         utilsCore.objectAssign(dicts[lang], dict);
       } else {
         dicts[lang] = utilsCore.objectAssign({}, dict);
       }
-      
+
       return this;
     },
-    
-    getDict(lang) {
-      lang = this.isSupport(lang) || this.getLang();
-      
+
+    getDict(_lang) {
+      const lang = this.isSupport(_lang) || this.getLang();
+
       return dicts[lang];
     },
 
-    setLang(lang) {
+    setLang(_lang) {
+      let lang = _lang;
+
       if (lang !== undefined) {
         lang = special[lang] || lang;
-        
-        
+
+
         if (!this.isSupport(lang)) {
           lang = localOptions.defaultLang;
         }
-        
+
         saveLangWithBrowserLocal(lang);
         localLang = lang;
         currDict = dicts[lang] || {};
       }
-      
+
       return this;
     },
-    
+
     getOptions() {
-      return  localOptions;
+      return localOptions;
     },
 
     getLang() {
-      var defLang = localOptions.defaultLang;
-      var local, start, end;
-      var ret = getLangWithBrowserLocal() || localLang;
-      
-      return this.isSupport(ret) || defLang;
+      const defLang = localOptions.defaultLang;
+      let ret = getLangWithBrowserLocal() || localLang;
+
+      ret = this.isSupport(ret) || defLang;
+
+      return ret;
     },
-    
+
     translate(str) {
       return currDict[str] || str;
-    }
+    },
 
-  });
+    init(options) {
+      let initOptions = {};
+      let initLang;
 
-  b28n.init = function(options) {
-    let initLang;
-    let initOptions = {};
-    
-    // Extend options
-    utilsCore.objectAssign(initOptions, DEFAULT_OPTIONS, options);
-    
-    // init lang
-    if(initOptions.lang) {
-      initLang = initOptions.lang;
-    } else {
-      initLang = getLangWithBrowserLocal();
-      
-      if(!initLang) {
-        initLang = initOptions.defaultLang;
-        
-        initLang = (win.navigator.language || win.navigator.userLanguage ||
+      // Extend options
+      initOptions = utilsCore.objectAssign({}, DEFAULT_OPTIONS, options);
+
+      // init lang
+      if (initOptions.lang) {
+        initLang = initOptions.lang;
+      } else {
+        initLang = getLangWithBrowserLocal();
+
+        if (!initLang) {
+          initLang = initOptions.defaultLang;
+
+          initLang = (win.navigator.language || win.navigator.userLanguage ||
             win.navigator.browserLanguage || win.navigator.systemLanguage ||
             initLang).toLowerCase();
 
-        initLang = special[initLang] || initLang.split("-")[0].toString();
+          initLang = special[initLang] || initLang.split('-')[0].toString();
+        }
       }
-    }
-    
-    initLang = isSupportLang(initLang, initOptions.supportLang) || initOptions.defaultLang;
-    
-    // 把初始化的语言保存在本地
-    saveLangWithBrowserLocal(initLang);
-    
-    localLang = initLang;
-    currDict = dicts[localLang] || {};
-  
-    localOptions = initOptions;
 
-    return this;
-  }
-  
+      initLang = isSupportLang(initLang, initOptions.supportLang) || initOptions.defaultLang;
+
+      // 把初始化的语言保存在本地
+      saveLangWithBrowserLocal(initLang);
+
+      localLang = initLang;
+      currDict = dicts[localLang] || {};
+
+      localOptions = initOptions;
+
+      return this;
+    },
+  });
+
   // 初始化默认字典
-  if(!currDict) {
-    currDict = dicts[b28n.getLang()] = {};
+  if (!currDict) {
+    currDict = dicts[localB28n.getLang()] = {};
   }
 
-  win.b28n = b28n;
-  
-  win._ = function(str) {
-    var translateStr = b28n.translate(str);
-    var args = [].slice.call(arguments);
-    var ret;
-    
-    if(arguments.length <= 1) {
+  win._ = function _(str, ...rest) {
+    const translateStr = localB28n.translate(str);
+    let ret;
+
+    if (rest.length === 0) {
       ret = translateStr;
     } else {
-      args.shift();
-      args.unshift(translateStr);
-      ret = strUtils.format.apply(strUtils, args);
+      ret = strUtils.format.call(strUtils, translateStr, ...rest);
     }
-    
+
     return ret;
-  }
-  
-  b28n.langMap = {
-		"cn": "简体中文",
-		"zh": "繁體中文",
-		"de": "Deutsch", //德语
-		"en": "English", //英语
-		"es": "Español", //西班牙
-		"fr": "Français", //法国
-		"hu": "Magyar", //匈牙利
-		"it": "Italiano", //意大利
-		"pl": "Polski", //波兰
-		"ro": "Română", //罗马尼亚
-		"ar": "العربية", //阿拉伯
-		"tr": "Türkçe", //土耳其
-		"ru": "Русский", //Russian	俄语
-		"pt": "Português" //Portugal 葡萄牙语
-	};
-  
-  return b28n;
-})(document, window);
+  };
+
+  localB28n.langMap = {
+    cn: '简体中文',
+    zh: '繁體中文',
+    de: 'Deutsch', // 德语
+    en: 'English', // 英语
+    es: 'Español', // 西班牙
+    fr: 'Français', // 法国
+    hu: 'Magyar', // 匈牙利
+    it: 'Italiano', // 意大利
+    pl: 'Polski', // 波兰
+    ro: 'Română', // 罗马尼亚
+    ar: 'العربية', // 阿拉伯
+    tr: 'Türkçe', // 土耳其
+    ru: 'Русский', // Russian	俄语
+    pt: 'Português', // Portugal 葡萄牙语
+  };
+
+  return localB28n;
+}(document, window));
+
+window.b28n = b28n;
 
 export default b28n;
