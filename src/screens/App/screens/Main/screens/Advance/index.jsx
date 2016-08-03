71f07c1d7cf26d62ae02bf86105e48a96265dd82
lichao <charles.li@axilspot.com>
2016-08-03 17:23:18 +0800

    高级设置页面HTML部分完成

--- src/screens/App/screens/Main/screens/Advance/index.jsx
+++ src/screens/App/screens/Main/screens/Advance/index.jsx
@@ -19,6 +19,7 @@ export const Advance = React.createClass({
   render() {
     return (
       <div className='advanceWrap'>
+
         <div className='rtsConfg'>
           <FormGroup
             label={_('RTS Switch') }
@@ -44,30 +45,159 @@ export const Advance = React.createClass({
             </label>
           </FormGroup>
           <FormGroup
+            className="rtsThresholdValue"
             label={_('RTS Threshold') }
             type="number"
-            help="范围："
+            help={_("Range: ")}
             required
           />
         </div>
 
         <div className="disConfg">
-          <span>{_("Distance Adjust Mode") }</span>
-          <FormInput name="distanceConfg" type="radio" id="autoDis" value='auto' />
-          <span htmlFor='rtsOn'>Auto Adjust</span>
-          <FormInput name="distanceConfg" type="radio" id="manuDis" value='manu'/>
-          <span htmlFor='rtsOff'>Manu Adjust</span>
-          <div className="disValue">
-            <span>{_("Distance Value") }</span>
-            <FormInput type="number" />m
-          </div>
+          <FormGroup
+            label={_("Distance Adjust Mode")}
+            type="checkbox"
+          >
+            <label htmlFor="autoDis">
+              <FormInput
+                name="distanceConfg"
+                type="radio"
+                id="autoDis"
+                value='auto'
+              />
+              <span>Auto Adjust</span>
+            </label>
+            <label htmlFor="manuDis">
+              <FormInput
+                name="distanceConfg"
+                type="radio"
+                id="manuDis"
+                value='manu'
+              />
+              <span>Manu Adjust</span>
+            </label>
+          </FormGroup>
         </div>
+
+          <div className="disValue">
+            <FormGroup
+              type="number"
+              label={_("Distance Value") }
+              help="M"
+            />
+          </div>
+
         <div className="clientIsoConfg">
-          <span>{_("Client Isolation") }</span>
-          <FormInput name="clientIsolateConfg" type="radio" id="isolateOn" value='on' />
-          <span htmlFor='isolateOn'>On</span>
-          <FormInput name="clientIsolateConfg" type="radio" id="isolateOff" value='off' defaultChecked/>
-          <span htmlFor='isolateOff'>Off</span>
+          <FormGroup
+            label={_("Client Isolation") }
+          >
+            <label htmlFor="isolateOn">
+              <FormInput
+                name="clientIsolateConfg"
+                type="radio"
+                id="isolateOn"
+                value='on'
+              />
+              <span>On</span>
+            </label>
+            <label htmlFor="isolateOff">
+              <FormInput
+                name="clientIsolateConfg"
+                type="radio"
+                id="isolateOff"
+                value='off'
+              />
+              <span>Off</span>
+            </label>
+          </FormGroup>
+        </div>
+
+        <div className="sensitivityConfg">
+          <FormGroup
+            label={_("Sensitivity Threshold") }
+          >
+            <label htmlFor="thresholdOn">
+              <FormInput
+                name="sensitivityThresConfg"
+                type="radio"
+                id="thresholdOn"
+                value='on'
+              />
+              <span>On</span>
+            </label>
+            <label htmlFor="thresholdOff">
+              <FormInput
+                name="sensitivityThresConfg"
+                type="radio"
+                id="thresholdOff"
+                value='off'
+              />
+              <span>Off</span>
+            </label>
+          </FormGroup>
+          <FormGroup
+            className="sensThresholdValue"
+            label={_('Threshold Value') }
+            type="number"
+            help={"dBm "+_("Range: ")+"-96 ~ 0"}
+            required
+           />
+        </div>
+
+        <div className="signalLedConfg">
+          <FormGroup label={_("Signal LED Thresholds") } >
+            <br/><br/>
+            <label htmlFor="threshdForLed1">
+              <span>LED1  </span>
+              <FormInput
+                className="threshdForLed"
+                id="threshdForLed1"
+                label="LED1"
+                type="number"
+                defaultValue="-94"
+              />
+              <span>{" "+"dbm"}</span>
+            </label>
+            <br/><br/>
+            <label htmlFor="threshdForLed1">
+              <span>LED2  </span>
+              <FormInput
+                className="threshdForLed"
+                id="threshdForLed2"
+                label="LED2"
+                type="number"
+                help="dbm"
+                defaultValue="-80"
+              />
+              <span>{" "+"dbm"}</span>
+            </label>
+            <br/><br/>
+            <label htmlFor="threshdForLed1">
+              <span>LED3  </span>
+              <FormInput
+                className="threshdForLed"
+                id="threshdForLed3"
+                label="LED3"
+                type="number"
+                help="dbm"
+                defaultValue="-73"
+              />
+              <span>{" "+"dbm"}</span>
+            </label>
+            <br/><br/>
+            <label htmlFor="threshdForLed1">
+              <span>LED4  </span>
+              <FormInput
+                className="threshdForLed"
+                id="threshdForLed4"
+                label="LED4"
+                type="number"
+                help="dbm"
+                defaultValue="-65"
+              />
+              <span>{" "+"dbm"}</span>
+            </label>
+          </FormGroup>
         </div>
 
       </div>
