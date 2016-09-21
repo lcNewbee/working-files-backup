e6f7dea96ee75be122269f70d085e234e0a231a7
xiechengyu <taijisanfeng@yahoo.com>
2016-09-21 16:37:22 +0800

    AXC: 添加Login界面

---
+++ php/application/controllers/Login.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	public function index() {
+    $result=array(
+      'state'=>array(
+        'code' => 2000,
+        'msg' => 'ok',
+      )
+    );
+    echo json_encode($result);
+	}
+}
+?>
