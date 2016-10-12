c464dad50ab3ed4699fffbd2ace1e336055624f1
xiechengyu <taijisanfeng@yahoo.com>
2016-10-12 11:15:10 +0800

    Common: 修改测试数据

--- tools/gulp/demo.js
+++ tools/gulp/demo.js
@@ -3,19 +3,24 @@ var gutil = require('gulp-util');
 var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
+var $ = require('gulp-load-plugins')();
 var paths = gulp.paths;
+var argv = require('minimist')(process.argv.slice(2));
 
 gulp.task('demo:copy', function() {
-  gulp.src(['./tools/data/axc/*.*', './tools/data/*.*'])
+  var name = 'ac';
+
+  if(argv.n) {
+    name = argv.n;
+  }
+  return gulp.src(['./tools/data/' + name + '/*.*', './tools/data/*.*'])
     .pipe(rename(function(path) {
       path.extname = '';
     }))
+    .on('end', function() {
+      gutil.log('拷贝Ajax测试文件', gutil.colors.magenta(name));
+    })
     .pipe(gulp.dest(paths.build + '/goform/'));
-});
-
-
-gulp.task('demo', function(callback) {
-  runSequence(['clean', 'build'], 'demo:copy', 'demo:serve', callback);
 });
 
 gulp.task('demo:serve', function(callback) {
@@ -32,4 +37,9 @@ gulp.task('demo:serve', function(callback) {
   });
 
   callback();
+});
+
+
+gulp.task('demo', function(callback) {
+  runSequence('clean', 'config', 'build', 'demo:copy', 'demo:serve', callback);
 });
