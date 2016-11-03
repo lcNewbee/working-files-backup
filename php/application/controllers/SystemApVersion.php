a3f0043fbee6e7fc03bdbdfc628dc40771626afb
zhangfang <fanny.zhang@axilspot.com>
2016-11-03 16:04:41 +0800

    AXC: AP版本

--- php/application/controllers/SystemApVersion.php
+++ php/application/controllers/SystemApVersion.php
@@ -36,7 +36,6 @@ class SystemApVersion extends CI_Controller {
     "nextPage"=>(int)element('nextPage',$_GET,-1),
     "lastPage"=>(int)element('lastPage',$_GET,1)
     );
-
 		$result=array(
 						      'state'=>$state,
 						      'data'=>array(
@@ -46,44 +45,114 @@ class SystemApVersion extends CI_Controller {
 						    );
 		return $result;
 	}
+   public function do_upload()
+    {
+    $config['upload_path'] = '/etc/Ap_ver';
+    $config['overwrite']=true;
+    $config['max_size'] = 0;
+    $config['allowed_types'] = '*';
 
+    $this->load->library('upload', $config);
+
+    if (!$this->upload->do_upload('versionFile')) {
+      $error = array('error' => $this->upload->display_errors());
+      $result = array(
+        'state'=>array(
+          'code'=>4000,
+          'msg'=>$error
+        )
+      );
+    } else {
+      $data = array('upload_data' => $this->upload->data());
+      $result = array(
+        'state'=>array(
+          'code'=>2000,
+          'msg'=>'OK'
+        ),
+        'data'=>$data
+      );
+    }
+		return $result;
+    }
 	function onAction($data) {
+    if (!$data) {
+      $data = $_POST;
+    }
 		$result = null;
 		$actionType = element('action', $data);
-    function getCgiParam($oriData) {
-      $retData = array(
-        'vendor'=>element('vendor',$oriData,48208),
-        'model'=>element('model', $oriData),
-        'hardware'=>element('hardware', $oriData,''),
-        'radionum'=>element('radionum', $oriData),
-        'vendor'=>element('vendor', $oriData),
-      );
-      return $retData;
-    }
-
+    $selectList = element('selectedList', $data);
+    // function getCgiParam($oriData) {
+    //   $retData = array(
+    //     'vendor'=>element('vendor',$oriData, 48208),
+    //     'model'=>element('model', $oriData),
+    //     'sfver'=>element('softVersion', $oriData),
+    //     'fmname'=>$filename,
+    //     'filepath'=>$filepath,
+    //     'active'=>element('active', $oriData)
+    //   );
+    //   return $retData;
+    // }
 		if ($actionType === 'add') {
-			$temp_data=getCgiParam($data);
-      // $result=json_encode($temp_data);
-      $result=axc_add_apfirmware(json_encode($temp_data));
+      $upload_data=$this->do_upload();
+      if($upload_data['state']['code']==2000){
+        $filename=$this->upload->data('file_name');
+        $filepath=$this->upload->data('full_path');
+         $retData = array(
+        'vendor'=>element('vendor',$data, 48208),
+        'model'=>element('model', $data),
+        'sfver'=>element('softVersion', $data),
+        'fmname'=>$filename,
+        'filepath'=>$filepath,
+        'active'=>element('active', $data)
+      );
+      }
+      $result=axc_add_apfirmware(json_encode($retData ));
 		}
-
-     elseif($actionType === 'active'){
-     $temp_data=getCgiParam($data);
-    $result=axc_active_apfirmware(json_encode($temp_data));
+    elseif($actionType === 'active'){
+    $upload_data=$this->do_upload();
+      if($upload_data['state']['code']==2000){
+        $filename=$this->upload->data('file_name');
+        $filepath=$this->upload->data('full_path');
+         $retData = array(
+        'vendor'=>element('vendor',$data, 48208),
+        'model'=>element('model', $data),
+        'sfver'=>element('softVersion', $data),
+        'fmname'=>$filename,
+        'filepath'=>$filepath,
+        'active'=>element('active', $data)
+      );
+      }
+     $result=axc_active_apfirmware(json_encode($retData));
     }
-
+    elseif($actionType === 'edit'){
+      $upload_data=$this->do_upload();
+      if($upload_data['state']['code']==2000){
+        $filename=$this->upload->data('file_name');
+        $filepath=$this->upload->data('full_path');
+         $retData = array(
+        'vendor'=>element('vendor',$data, 48208),
+        'model'=>element('model', $data),
+        'sfver'=>element('softVersion', $data),
+        'fmname'=>$filename,
+        'filepath'=>$filepath,
+        'active'=>element('active', $data)
+      );
+      }
+      $result=axc_modify_apfirmware(json_encode($retData));
+    }
     elseif($actionType === 'delete'){
-      $temp_data=array(
-        'name'=>element('name',$data['selectedList']),
-       );
-      $state=axc_del_apfirmware(json_encode($temp_data));
-       $result=$state;
-    }
+      foreach($selectList as $item) {
+          $deleteItem = array(
+            'model'=>element('model', $item),
+            'sfver'=>element('softVersion', $item),
+          );
+        $result=axc_del_apfirmware(json_encode($deleteItem));
+      }
+	  }
 
-		return $result;
-	}
-
-	public function index() {
+    return $result;
+  }
+	public function index(){
 		$result = null;
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$data = json_decode(file_get_contents("php://input"), true);
