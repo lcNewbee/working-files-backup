6f40ca251612ca944f9edd98bf1d2e371b61a9f9
lichao <charles.li@axilspot.com>
2016-12-02 10:40:56 +0800

    合并冲突

--- src/index.jsx
+++ src/index.jsx
@@ -19,20 +19,8 @@ const hashHistory = ReactRouter.hashHistory;
 const mountNode = document.getElementById('app');
 
 // 引入产品配置
-<<<<<<< c64286f1c5872e593284587d20a1511fb440e642
-<<<<<<< d9dc1cdf1da216ae5a6594e1c7d582052239a49c
-<<<<<<< 97e60cfc383ff0cf81edc37b05b9d52ecddd4c74
 const renderApp = () => {
-  const prodConfig = require('./config/axc');
-=======
-const prodConfig = require('./config/aip5');
->>>>>>> 平台添加新功能，并修改部分bug
-=======
-const prodConfig = require('./config/aip10');
->>>>>>> AP：解BUG & 平台继续添加新功能
-=======
-const prodConfig = require('./config/aip5');
->>>>>>> Ap:添加AP发布的gulp命令
+  const prodConfig = require('./config/aip5');
 
   // 主渲染入口
   ReactDOM.render(
@@ -47,21 +35,13 @@ const prodConfig = require('./config/aip5');
 
 // Enable hot reload by react-hot-loader
 if (module.hot) {
-<<<<<<< 97e60cfc383ff0cf81edc37b05b9d52ecddd4c74
   const reRenderApp = () => {
     try {
       renderApp();
     } catch (error) {}
   };
-=======
-  // Enable Webpack hot module replacement for reducers
+
   module.hot.accept('./config/aip5', () => {
-    const newConfig = require('./config/aip5');
-
-    const nextRootReducer = combineReducers(newConfig.reducers);
->>>>>>> 平台添加新功能，并修改部分bug
-
-  module.hot.accept('./config/axc', () => {
     setImmediate(() => {
       // Preventing the hot reloading error from react-router
       unmountComponentAtNode(mountNode);
