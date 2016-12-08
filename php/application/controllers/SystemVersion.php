5ea3f3d36cd1a5422c3b6dd9419fc46b6536ac0c
yang-xing-hua <yangxinghua_net@163.com>
2016-12-08 10:42:14 +0800

    AXC 解决AC升级上传错误固件 升级时依然提示成功bug

--- php/application/controllers/SystemVersion.php
+++ php/application/controllers/SystemVersion.php
@@ -2,90 +2,83 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 // require_once('/libraries/Response.php');
 class SystemVersion extends CI_Controller {
-  public $acVersion = '';
-  public $acVersionUses = '0';
+	public $acVersion = '';
+	public $acVersionUses = '0';
 	public function __construct() {
 		parent::__construct();
 		$this->load->database();
 		$this->load->helper('array');
 	}
 	function onUpgradeAc() {
-    $config['upload_path'] = '/etc/Ac_ver/';
-    $config['overwrite']=true;
-    $config['max_size'] = 0;
-    $config['allowed_types'] = '*';
-
-    $this->load->library('upload', $config);
-
-    if (!$this->upload->do_upload('versionFile')) {
-      $error = array('error' => $this->upload->display_errors());
-      $result = array(
-        'state'=>array(
-          'code'=>4000,
-          'msg'=>$error
-        )
-      );
-    } else {
-      $data = array('upload_data' => $this->upload->data());
-      //$this->acVersion = $data['name'];
-      $result = array(
-        'state'=>array(
-          'code'=>2000,
-          'msg'=>$data
-        )
-      );
-    }
+		$config['upload_path'] = '/etc/Ac_ver/';
+		$config['overwrite'] = true;
+		$config['max_size'] = 0;
+		$config['allowed_types'] = '*';
+		$this->load->library('upload', $config);
+		if (!$this->upload->do_upload('versionFile')) {
+			$error = array('error' => $this->upload->display_errors());
+			$result = array(
+                'state' => array(
+                    'code' => 4000, 
+                    'msg' => $error
+                )
+            );
+		} else {
+			$data = array('upload_data' => $this->upload->data());
+			//$this->acVersion = $data['name'];
+			$result = array('state' => array('code' => 2000, 'msg' => $data));
+		}
 		return $result;
 	}
-
 	public function index() {
 		$result = null;
 		echo $result;
 	}
-  public function upload() {
+	public function upload() {
 		$result = array(
-      'state'=>array(
-        'code'=>3002,
-        'msg'=>'Need Post'
-      )
-    );
+            'state' => array(
+                'code' => 3002, 
+                'msg' => 'Need Post'
+            )
+        );
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$result = $this->onUpgradeAc();
 		}
-
-    echo json_encode($result);
+		echo json_encode($result);
 	}
-  public function upgrade() {
+	public function upgrade() {
 		$result = array(
-      'state'=>array(
-        'code'=>3002,
-        'msg'=>'Need Post'
-      )
-    );
+            'state' => array(
+                'code' => 3002, 
+                'msg' => 'Need Post'
+            )
+        );
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      $data = json_decode(file_get_contents("php://input"), true);
-      $filename = element('filename', $data, '');
+			$data = json_decode(file_get_contents("php://input"), true);
+			$filename = element('filename', $data, '');
 			$msg = 'ok';
-      $result['state']['msg'] = $msg;
-
-      echo json_encode($result);
-
-      sleep(1);
-      $msg = shell_exec('sysupgrade -f /etc/Ac_ver/'.$filename);
+			$result['state']['msg'] = $msg;
+			sleep(1);
+			$msg = shell_exec('sysupgrade -f /etc/Ac_ver/' . $filename);
+			$needle = 'sysupgrade fail';
+			if (strpos($msg, $needle)) {
+				$result['state']['code'] = 6300;
+				$result['state']['msg'] = 'Firmware error upgrade failed';
+			}
+			echo json_encode($result);
 		}
 	}
-  public function backup() {
+	public function backup() {
 		$result = array(
-      'state'=>array(
-        'code'=>3002,
-        'msg'=>'Need Post'
-      )
-    );
+            'state' => array(
+                'code' => 3002, 
+                'msg' => 'Need Post'
+            )
+        );
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$msg = shell_exec('');
 		}
-    $result['state']['msg'] = $msg;
-
-    echo json_encode($result);
+		$result['state']['msg'] = $msg;
+		echo json_encode($result);
 	}
 }
