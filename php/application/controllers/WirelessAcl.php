f42c0455dc4bc220b6589b2fc0e26f0468ba1e7a
yang-xing-hua <yangxinghua_net@163.com>
2016-12-22 11:54:36 +0800

    Axc 提交ACL  web操作日志记录

--- php/application/controllers/WirelessAcl.php
+++ php/application/controllers/WirelessAcl.php
@@ -1,45 +1,45 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class WirelessAcl extends CI_Controller {
-	public function __construct() {
-		parent::__construct();
-		$this->load->database();
-		$this->load->helper('array');
-		$this->load->model('group/WirelessAcl_Model');
-	}
-	function fetch() {
-		$retdata = array(
-			'groupid' => (int)element('groupid', $_GET, -1),
-			'filterGroupid' => (int)element('filterGroupid', $_GET, -1),
-			'acltype' => element('aclType', $_GET, 'black')			
-		);
-		return $this->WirelessAcl_Model->get_acl_list($retdata);
-	}
-	function onAction($data) {
-		$result = null;
-		$actionType = element('action', $data);
-		if ($actionType === 'add') {
-			return $this->WirelessAcl_Model->add_acl($data);
-		} elseif ($actionType === 'edit') {
-			//$result=axc_set_wireless_acl(json_encode($data));			
-		} elseif ($actionType === 'delete') {
-			return $this->WirelessAcl_Model->delete_acl($data);
-		} elseif ($actionType === 'setting') {
-			return $this->WirelessAcl_Model->other_config($data);
-		} elseif ($actionType === 'copy') {
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+        $this->load->helper('array');
+        $this->load->model('group/WirelessAcl_Model');
+    }
+    function fetch() {
+        $retdata = array(
+            'groupid' => (int)element('groupid', $_GET, -1),
+            'filterGroupid' => (int)element('filterGroupid', $_GET, -1),
+            'acltype' => element('aclType', $_GET, 'black')			
+        );
+        return $this->WirelessAcl_Model->get_acl_list($retdata);
+    }
+    function onAction($data) {
+        $result = null;
+        $actionType = element('action', $data);
+        if ($actionType === 'add') {
+            return $this->WirelessAcl_Model->add_acl($data);
+        } elseif ($actionType === 'edit') {
+            //$result=axc_set_wireless_acl(json_encode($data));			
+        } elseif ($actionType === 'delete') {
+            return $this->WirelessAcl_Model->delete_acl($data);
+        } elseif ($actionType === 'setting') {
+            return $this->WirelessAcl_Model->other_config($data);
+        } elseif ($actionType === 'copy') {
             return $this->WirelessAcl_Model->copy_config($data);
         }
-		return $result;
-	}
-	public function index() {
-		$result = null;
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$data = json_decode(file_get_contents("php://input"), true);
-			$result = $this->onAction($data);
-			echo $result;
-		} else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-			$result = $this->fetch();
-			echo $result;
-		}
-	}
+        return $result;
+    }
+    public function index() {
+        $result = null;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $this->onAction($data);
+            echo $result;
+        } else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $result = $this->fetch();
+            echo $result;
+        }
+    }
 }
--- php/application/models/group/WirelessAcl_Model.php
+++ php/application/models/group/WirelessAcl_Model.php
@@ -1,115 +1,152 @@
 <?php
 class WirelessAcl_Model extends CI_Model {
-	public function __construct() {
-		parent::__construct();
-		$this->load->database();
-		$this->load->helper(array('array', 'my_customfun_helper'));
-		$this->load->library('SqlPage');
-	}	
-	public function get_acl_list($retdata) {		
-		$result = null;		
-		//所有组
-		if($retdata['groupid'] === -100) {
-			$result = $this->get_acl_data($retdata['filterGroupid'],$retdata['acltype']);
-		}else{			
-			$acltype = 'black';			
-			$querydata = $this->db->select('ap_group.id,wids_template.acltype')
-									->from('ap_group')
-									->join('wids_template','ap_group.wids_tmp_id=wids_template.id','left')
-									->where("ap_group.id=".$retdata['groupid'])
-									->get()->result_array();
-			if( count($querydata) > 0) {
-				$acltype = $querydata[0]['acltype'];		
-			}
-			$result = axc_get_wireless_acl(json_encode($retdata));	
-			$cgidata = json_decode($result);			
-			$cgidata->data->settings = array('type'=>$acltype);
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->database();
+        $this->load->helper(array('array', 'my_customfun_helper'));
+        $this->load->library('SqlPage');
+    }	
+    public function get_acl_list($retdata) {		
+        $result = null;		
+        //所有组
+        if($retdata['groupid'] === -100) {
+            $result = $this->get_acl_data($retdata['filterGroupid'],$retdata['acltype']);
+        }else{			
+            $acltype = 'black';			
+            $querydata = $this->db->select('ap_group.id,wids_template.acltype')
+                                    ->from('ap_group')
+                                    ->join('wids_template','ap_group.wids_tmp_id=wids_template.id','left')
+                                    ->where("ap_group.id=".$retdata['groupid'])
+                                    ->get()->result_array();
+            if( count($querydata) > 0) {
+                $acltype = $querydata[0]['acltype'];		
+            }
+            $result = axc_get_wireless_acl(json_encode($retdata));	
+            $cgidata = json_decode($result);			
+            $cgidata->data->settings = array('type'=>$acltype);
 
-			$result = $cgidata;
-		}
-		return json_encode($result);
-	}
-	public function add_acl($data) {
-		$result = null;
-		if (!empty($data['groupid'])) {
-			$cgiary['groupid'] = (int)$data['groupid'];
-			$cgiary['mac'] = (string)$data['mac'];
-			$cgiary['reason'] = (string)$data['reason'];
-			$result = axc_set_wireless_acl(json_encode($cgiary));
-		}
-		return $result;
-	}
-	public function copy_config($data) {
-		$maclist = $data['copySelectedList'];
-		if(is_array($maclist)) {
-			$arr['groupid'] = (int)element('groupid', $data, -1);
-			foreach ($maclist as $mac) {
-				$arr['mac'] = $mac;
-				$arr['reason'] = 'static';
-				axc_set_wireless_acl(json_encode($arr));
-			}
-		}
-		return json_encode(json_ok());
-		
-	}
-	public function delete_acl($data) {
-		$result = null;
-		if (!empty($data['groupid'])) {
-			$detary = array();
-			$detary['groupid'] = $data['groupid'];
-			foreach( $data['selectedList'] as $ary){
-				$detary['mac'] = $ary['mac'];
-				axc_del_wireless_acl(json_encode($detary));
-			}
-			$result = json_encode(array('state' => array('code' => 2000, 'msg' => 'ok')));
-		}
-		return $result;
-	}
-	public function other_config($data) {
-		$result = null;
-		if(!empty($data['groupid'])){
-			$cgi_dyblk['groupid'] = (int)$data['groupid'];
-			$cgi_dyblk['type'] = (string)$data['type'];
-			$result = axc_change_wireless_acltype(json_encode($cgi_dyblk));
-		}
-		return $result;
-	}
-	/*********************
-	 * @filterid 过滤groupid
-	 * @type     获取类型 默认白
-	*********************/
-	public function get_acl_data($filterid=0,$type='black') {
-		$tableName = 'sta_black_list';
-		if( $type === 'white') {
-			$tableName = 'sta_white_list';
-		}
-		$result = array(
-			'state'=>array('code'=>2000,'msg'=>'ok'),
-			'data'=>array(
-				'list'=>array()
-			)
-		);
-		$querydata = $this->db->select('mac,vendor,clientType,reason,lastTime')
-								->from($tableName)
-								->where('wids_id !=',$filterid)
-								->get()->result_array();
-		if(count($querydata) > 0){
-			$macary = array();
-			foreach ($querydata as $row) {
-				array_push($macary,$row['mac']);
-			}
-			//所有组去除重复
-			$retdata = array();
-			$macary = array_unique($macary);
-			foreach ($macary as $res) {
-				$queryrow = $this->db->select('mac,vendor,clientType,reason,lastTime')
-									->from($tableName)
-									->where('mac',$res)
-									->get()->result_array();
-				$retdata[] = $queryrow[0];
-			}			
-			$result['data']['list'] = $retdata;				
-		}
-		return $result;
-	}
+            $result = $cgidata;
+        }
+        return json_encode($result);
+    }
+    public function add_acl($data) {
+        $result = null;
+        if (!empty($data['groupid'])) {
+            $cgiary['groupid'] = (int)$data['groupid'];
+            $cgiary['mac'] = (string)$data['mac'];
+            $cgiary['reason'] = (string)$data['reason'];
+            $result = axc_set_wireless_acl(json_encode($cgiary));
+            //log
+            $cgiObj = json_decode($result);			
+            if( is_object($cgiObj) && $cgiObj->state->code === 2000) {         
+                $logary = array(
+                    'type'=>'Add',
+                    'operator'=>element('username',$_SESSION,''),
+                    'operationCommand'=>"Add Acl ".$cgiary['mac']." Target group id=".$cgiary['groupid'],
+                    'operationResult'=>'ok',
+                    'description'=>""
+                );
+                Log_Record($this->db,$logary);
+            }
+        }
+        return $result;
+    }
+    public function copy_config($data) {
+        $maclist = $data['copySelectedList'];
+        if(is_array($maclist)) {
+            $arr['groupid'] = (int)element('groupid', $data, -1);
+            foreach ($maclist as $mac) {
+                $arr['mac'] = $mac;
+                $arr['reason'] = 'static';
+                $cgistr = axc_set_wireless_acl(json_encode($arr));
+                //log
+                $cgiObj = json_decode($cgistr);			
+                if( is_object($cgiObj) && $cgiObj->state->code === 2000) {         
+                    $logary = array(
+                        'type'=>'Add',
+                        'operator'=>element('username',$_SESSION,''),
+                        'operationCommand'=>"Add Acl ".$arr['mac']." Target group id=".$arr['groupid'],
+                        'operationResult'=>'ok',
+                        'description'=>""
+                    );
+                    Log_Record($this->db,$logary);
+                }
+            }
+        }
+        return json_encode(json_ok());
+        
+    }
+    public function delete_acl($data) {
+        $result = null;
+        if (!empty($data['groupid'])) {
+            $detary = array();
+            $detary['groupid'] = $data['groupid'];
+            foreach( $data['selectedList'] as $ary){
+                $detary['mac'] = $ary['mac'];
+                $cagistr = axc_del_wireless_acl(json_encode($detary));
+                //log
+                $cgiObj = json_decode($cagistr);			
+                if( is_object($cgiObj) && $cgiObj->state->code === 2000) {         
+                    $logary = array(
+                        'type'=>'Delete',
+                        'operator'=>element('username',$_SESSION,''),
+                        'operationCommand'=>"Delete Acl ".$cgiary['mac']." Target group id=".$cgiary['groupid'],
+                        'operationResult'=>'ok',
+                        'description'=>""
+                    );
+                    Log_Record($this->db,$logary);
+                }
+            }
+            $result = json_encode(array('state' => array('code' => 2000, 'msg' => 'ok')));
+        }
+        return $result;
+    }
+    public function other_config($data) {
+        $result = null;
+        if(!empty($data['groupid'])){
+            $cgi_dyblk['groupid'] = (int)$data['groupid'];
+            $cgi_dyblk['type'] = (string)$data['type'];
+            $result = axc_change_wireless_acltype(json_encode($cgi_dyblk));
+        }
+        return $result;
+    }
+    /*********************
+     * @filterid 过滤groupid
+     * @type     获取类型 默认白
+    *********************/
+    public function get_acl_data($filterid=0,$type='black') {
+        $tableName = 'sta_black_list';
+        if( $type === 'white') {
+            $tableName = 'sta_white_list';
+        }
+        $result = array(
+            'state'=>array('code'=>2000,'msg'=>'ok'),
+            'data'=>array(
+                'list'=>array()
+            )
+        );
+        $querydata = $this->db->select('mac,vendor,clientType,reason,lastTime')
+                                ->from($tableName)
+                                ->where('wids_id !=',$filterid)
+                                ->get()->result_array();
+        if(count($querydata) > 0){
+            $macary = array();
+            foreach ($querydata as $row) {
+                array_push($macary,$row['mac']);
+            }
+            //所有组去除重复
+            $retdata = array();
+            $macary = array_unique($macary);
+            foreach ($macary as $res) {
+                $queryrow = $this->db->select('mac,vendor,clientType,reason,lastTime')
+                                    ->from($tableName)
+                                    ->where('mac',$res)
+                                    ->get()->result_array();
+                $retdata[] = $queryrow[0];
+            }			
+            $result['data']['list'] = $retdata;				
+        }
+        return $result;
+    }
 }
