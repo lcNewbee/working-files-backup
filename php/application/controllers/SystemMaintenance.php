839ccc826a32b4aaad0c9e72077957103371e238
yang-xing-hua <yangxinghua_net@163.com>
2016-12-22 17:13:33 +0800

    Axc 更新系统维护，操作重启及备份配置时web日志记录

--- php/application/controllers/SystemMaintenance.php
+++ php/application/controllers/SystemMaintenance.php
@@ -1,49 +1,63 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class SystemMaintenance extends CI_Controller {
-	public function __construct() {
-		parent::__construct();
-		$this->load->database();
-		$this->load->helper('array');				
-	}
-	function fetch() {
-        /*
-		$retdata = array('page' => (int)element('page', $_GET, -1), 'size' => (int)element('size', $_GET, -1));
-		return $this->Log_Model->get_log_list($retdata);
-        */
-	}
-	function onAction($data) {
-		$result = null;
-		$actionType = element('action', $data);
-		if ($actionType === 'delete') {
-			$result = $this->Log_Model->log_delete($data);
-		} elseif ($actionType === 'setting') {
-			$result = $this->Log_Model->log_cfg($data);
-		}
-		return $result;
-	}
-	public function index() {
-		$result = null;
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$data = json_decode(file_get_contents("php://input"), true);
-			$result = $this->onAction($data);
-			echo $result;
-		} else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-			$result = $this->fetch();
-			echo $result;
-		}
-	}
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->database();
+        $this->load->helper(array('array', 'my_customfun_helper'));			
+    }
+    function fetch() {
+        /**/
+    }
+    function onAction($data) {
+        $result = null;
+        $actionType = element('action', $data);
+        if ($actionType === 'delete') {
+            $result = $this->Log_Model->log_delete($data);
+        } elseif ($actionType === 'setting') {
+            $result = $this->Log_Model->log_cfg($data);
+        }
+        return $result;
+    }
+    public function index() {
+        $result = null;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $this->onAction($data);
+            echo $result;
+        } else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $result = $this->fetch();
+            echo $result;
+        }
+    }
     public function backup() {
+        $logary = array(
+            'type'=>'Backup',
+            'operator'=>element('username',$_SESSION,''),
+            'operationCommand'=>"Backup AC config",
+            'operationResult'=>'ok',
+            'description'=>""
+        );
+        Log_Record($this->db,$logary);
         //系统备份        
         //copy('/var/run/config.db','/var/conf/config.db');
         exec('cp /var/run/config.db /var/conf/config.db');
-		//download
-		$this->load->helper('download');
-		$data = file_get_contents("/var/conf/config.db");
-		$name = 'config.db';
-		force_download($name, $data);
+        //download
+        $this->load->helper('download');
+        $data = file_get_contents("/var/conf/config.db");
+        $name = 'config.db';
+        force_download($name, $data);
     }
     public function reboot() {
+        $logary = array(
+            'type'=>'Reboot',
+            'operator'=>element('username',$_SESSION,''),
+            'operationCommand'=>"Reboot AC",
+            'operationResult'=>'ok',
+            'description'=>""
+        );
+        Log_Record($this->db,$logary);
         //重启系统
         exec('/sbin/reboot');
     }
