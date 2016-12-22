d48bd5d7a6e56b5745c55003096f6ccb884cb9ae
yang-xing-hua <yangxinghua_net@163.com>
2016-12-22 18:21:11 +0800

    Axc 优化AP 型号及版本日志记录

--- php/application/controllers/SystemApVersion.php
+++ php/application/controllers/SystemApVersion.php
@@ -3,207 +3,223 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class SystemApVersion extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
+        $this->load->library('session');
 		$this->load->database();
-		$this->load->helper('array');
+		$this->load->helper(array('array', 'my_customfun_helper'));
 	}
-	function fetch(){
-		$query=$this->db->select('id,model,subversion,fm_name,upd_path,active')
-              ->from('ap_firmware')
-              ->get()->result_array();
-   $keys = array(
-      'id'=>'id',
-      'model'=> 'model',
-      'subversion'=>'softVersion',
-      'fm_name'=>'fileName',
-      'upd_path'=>'uploadPath',
-      'active'=>'active'
-    );
-    $newArray = array();
-    foreach($query as $key=>$val) {
-      $newArray[$key] = array();
-      foreach($val as $k=>$v) {
-        $newArray[$key][$keys[$k]] = $v;
-      }
-    }
-		$state=array(
-				      'code'=>2000,
-				      'msg'=>'OK'
-				    );
-    $page=array(
-    "start"=>(int) element('start',$_GET,1),
-    "size"=> (int)element('size',$_GET,20),
-    "currPage"=>(int)element('currPage',$_GET,1),
-    "totalPage"=>(int)element('totalPage',$_GET,1),
-    "total"=>(int)element('total',$_GET,11),
-    "pageCount"=>(int)element('pageCount',$_GET,11),
-    "nextPage"=>(int)element('nextPage',$_GET,-1),
-    "lastPage"=>(int)element('lastPage',$_GET,1)
-    );
-		$result=array(
-						      'state'=>$state,
-						      'data'=>array(
-                  'page'=>$page,
-						      'list'=>$newArray
-						      )
-						    );
-		return $result;
-	}
-   public function do_upload()
-    {
-    $config['upload_path'] = '/etc/Ap_ver';
-    $config['overwrite']=true;
-    $config['max_size'] = 0;
-    $config['allowed_types'] = '*';
-
-    $this->load->library('upload', $config);
-
-    if (!$this->upload->do_upload('versionFile')) {
-      $error = array('error' => $this->upload->display_errors());
-      $result = array(
-        'state'=>array(
-          'code'=>4000,
-          'msg'=>$error
-        )
-      );
-    } else {
-      $data = array('upload_data' => $this->upload->data());
-      $result = array(
-        'state'=>array(
-          'code'=>2000,
-          'msg'=>'OK'
-        ),
-        'data'=>$data
-      );
-    }
-		return $result;
-    }
-	function onAction($data) {
-    if (!$data) {
-      $data = $_POST;
-    }
-		$result = null;
-		$actionType = element('action', $data);
-    $selectList = element('selectedList', $data);
-    // function getCgiParam($oriData) {
-    //   $retData = array(
-    //     'vendor'=>element('vendor',$oriData, 48208),
-    //     'model'=>element('model', $oriData),
-    //     'sfver'=>element('softVersion', $oriData),
-    //     'fmname'=>$filename,
-    //     'filepath'=>$filepath,
-    //     'active'=>element('active', $oriData)
-    //   );
-    //   return $retData;
-    // }
-		if ($actionType === 'add') {
-      $upload_data=$this->do_upload();
-      if($upload_data['state']['code']==2000){
-        $filename=$this->upload->data('file_name');
-        $filepath=$this->upload->data('full_path');
-         $retData = array(
-        'vendor'=>element('vendor',$data, 48208),
-        'model'=>element('model', $data),
-        'sfver'=>element('softVersion', $data),
-        'fmname'=>$filename,
-        'filepath'=>$filepath,
-        'active'=>(int)element('active', $data,0)
-      );
-      }
-      $result=axc_add_apfirmware(json_encode($retData ));
-		}
-    elseif($actionType === 'active'){
-      $retData = array(
-        'vendor'=>element('vendor',$data, 48208),
-        'model'=>element('model', $data),
-        'sfver'=>element('softVersion', $data),
-        'fmname'=>element('fileName', $data),
-        'filepath'=>element('uploadPath', $data),
-        'active'=>(int)element('active', $data,0)
-      );
-      $query_active=$this->db->select('id,active')
-          ->from('ap_firmware')
-          ->where('id',$item['id'])
-          ->get()->result_array();
-        if($query_active['0']['active']==0){
-          $result=axc_active_apfirmware(json_encode($retData));
+    function fetch(){
+        $query=$this->db->select('id,model,subversion,fm_name,upd_path,active')
+                        ->from('ap_firmware')
+                        ->get()->result_array();
+        $keys = array(
+            'id'=>'id',
+            'model'=> 'model',
+            'subversion'=>'softVersion',
+            'fm_name'=>'fileName',
+            'upd_path'=>'uploadPath',
+            'active'=>'active'
+        );
+        $newArray = array();
+        foreach($query as $key=>$val) {
+            $newArray[$key] = array();
+            foreach($val as $k=>$v) {
+                $newArray[$key][$keys[$k]] = $v;
+            }
         }
-        else{
-         $state=array(
-				      'code'=>6000,
-				      'msg'=>"The version of the related model you select is actived, cancellation  option doesn't work!you should active another version when you want to  cancel the current version!"
-				    );
-            $result=$state;
-        }
+        $state=array(
+            'code'=>2000,
+            'msg'=>'OK'
+        );
+        $page=array(
+            "start"=>(int) element('start',$_GET,1),
+            "size"=> (int)element('size',$_GET,20),
+            "currPage"=>(int)element('currPage',$_GET,1),
+            "totalPage"=>(int)element('totalPage',$_GET,1),
+            "total"=>(int)element('total',$_GET,11),
+            "pageCount"=>(int)element('pageCount',$_GET,11),
+            "nextPage"=>(int)element('nextPage',$_GET,-1),
+            "lastPage"=>(int)element('lastPage',$_GET,1)
+        );
+        $result=array(
+            'state'=>$state,
+            'data'=>array(
+                'page'=>$page,
+                'list'=>$newArray
+                )
+        );
+        return $result;
     }
-    elseif($actionType === 'edit'){
-      $upload_data=$this->do_upload();
-      if($upload_data['state']['code']==2000){
-        $filename=$this->upload->data('file_name');
-        $filepath=$this->upload->data('full_path');
-         $retData = array(
-        'vendor'=>element('vendor',$data, 48208),
-        'model'=>element('model', $data,''),
-        'sfver'=>element('softVersion', $data,''),
-        'fmname'=>$filename,
-        'filepath'=>$filepath,
-        'active'=>(int)element('active', $data,0)
-      );
-      }
-      $result=axc_modify_apfirmware(json_encode($retData));
-    }
-    elseif($actionType === 'delete'){
+    public function do_upload(){
+        $config['upload_path'] = '/etc/Ap_ver';
+        $config['overwrite']=true;
+        $config['max_size'] = 0;
+        $config['allowed_types'] = '*';
 
-      foreach($selectList as $item) {
-          $deleteItem = array(
-            'model'=>element('model', $item),
-            'sfver'=>element('softVersion', $item),
-          );
-          $query_active=$this->db->select('id,active')
-            ->from('ap_firmware')
-            ->where('id',$item['id'])
-            ->get()->result_array();
-          if($query_active['0']['active']==1){
-            $state=array(
-				      'code'=>6000,
-				      'msg'=>"The current version is actived,you can't delete it. If you want to delete it,please active another version!"
-				    );
-            $result=$state;
-          }
-          else{
-            $del_result=axc_del_apfirmware(json_encode($deleteItem));
-            $del_result_array=json_decode($del_result,true);
-            if ($del_result_array['state']['code']==2000){
-               $file=$item['uploadPath'];
-                $delete_file=@unlink ($file);
-                if($delete_file == true){
-                 $result=$del_result;
-                }
-                else{
-                   $state=array(
-                      'code'=>6000,
-				              'msg'=>'fail to delete the file in the folder!'
-                   );
-                   $result=$state;
+        $this->load->library('upload', $config);
+        if (!$this->upload->do_upload('versionFile')) {
+            $error = array('error' => $this->upload->display_errors());
+            $result = array(
+                'state'=>array(
+                    'code'=>4000,
+                    'msg'=>$error
+                )
+            );
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+            $result = array(
+                'state'=>array(
+                    'code'=>2000,
+                    'msg'=>'OK'
+                ),
+                'data'=>$data
+            );
+        }
+        return $result;
+    }
+    function onAction($data) {
+        if (!$data) {
+            $data = $_POST;
+        }
+        $result = null;
+        $actionType = element('action', $data);
+        $selectList = element('selectedList', $data);
+        if ($actionType === 'add') {
+            $upload_data=$this->do_upload();
+            if($upload_data['state']['code']==2000){
+                $filename=$this->upload->data('file_name');
+                $filepath=$this->upload->data('full_path');
+                $retData = array(
+                    'vendor'=>element('vendor',$data, 48208),
+                    'model'=>element('model', $data),
+                    'sfver'=>element('softVersion', $data),
+                    'fmname'=>$filename,
+                    'filepath'=>$filepath,
+                    'active'=>(int)element('active', $data,0)
+                );
+            }
+            $result=axc_add_apfirmware(json_encode($retData ));
+            //log
+            $cgiObj = json_decode($result);			
+            if( is_object($cgiObj) && $cgiObj->state->code === 2000) {
+                $logary = array(
+                    'type'=>'Add',
+                    'operator'=>element('username',$_SESSION,''),
+                    'operationCommand'=>"Add AP Version ".$retData['vendor'],
+                    'operationResult'=>'ok',
+                    'description'=>""
+                );
+                Log_Record($this->db,$logary);
+            }
+        } elseif ($actionType === 'active'){
+            $retData = array(
+                'vendor'=>element('vendor',$data, 48208),
+                'model'=>element('model', $data),
+                'sfver'=>element('softVersion', $data),
+                'fmname'=>element('fileName', $data),
+                'filepath'=>element('uploadPath', $data),
+                'active'=>(int)element('active', $data,0)
+            );
+            $query_active=$this->db->select('id,active')
+                                    ->from('ap_firmware')
+                                    ->where('id',$item['id'])
+                                    ->get()->result_array();
+            if($query_active['0']['active']==0){
+                $result=axc_active_apfirmware(json_encode($retData));
+            } else {
+                $state=array(
+                    'code'=>6000,
+                    'msg'=>"The version of the related model you select is actived, cancellation  option doesn't work!you should active another version when you want to  cancel the current version!"
+                );
+                $result=$state;
+            }
+        } elseif ($actionType === 'edit'){
+            $upload_data=$this->do_upload();
+            if($upload_data['state']['code']==2000){
+                $filename=$this->upload->data('file_name');
+                $filepath=$this->upload->data('full_path');
+                $retData = array(
+                    'vendor'=>element('vendor',$data, 48208),
+                    'model'=>element('model', $data,''),
+                    'sfver'=>element('softVersion', $data,''),
+                    'fmname'=>$filename,
+                    'filepath'=>$filepath,
+                    'active'=>(int)element('active', $data,0)
+                );
+            }
+            $result=axc_modify_apfirmware(json_encode($retData));
+            //log
+            $cgiObj = json_decode($result);			
+            if( is_object($cgiObj) && $cgiObj->state->code === 2000) {
+                $logary = array(
+                    'type'=>'Update',
+                    'operator'=>element('username',$_SESSION,''),
+                    'operationCommand'=>"Update AP Version ".$retData['vendor'],
+                    'operationResult'=>'ok',
+                    'description'=>""
+                );
+                Log_Record($this->db,$logary);
+            }
+        } elseif ($actionType === 'delete'){
+            foreach($selectList as $item) {
+                $deleteItem = array(
+                    'model'=>element('model', $item),
+                    'sfver'=>element('softVersion', $item),
+                );
+                $query_active=$this->db->select('id,active')
+                                        ->from('ap_firmware')
+                                        ->where('id',$item['id'])
+                                        ->get()->result_array();
+                if($query_active['0']['active']==1){
+                    $state=array(
+                        'code'=>6000,
+                        'msg'=>"The current version is actived,you can't delete it. If you want to delete it,please active another version!"
+                    );
+                    $result=$state;
+                } else {
+                    $del_result=axc_del_apfirmware(json_encode($deleteItem));
+                    $del_result_array=json_decode($del_result,true);
+                    //log
+                    $cgiObj = json_decode($result);			
+                    if( is_array($del_result_array) && $del_result_array['state']['code']==2000) {
+                        $logary = array(
+                            'type'=>'Delete',
+                            'operator'=>element('username',$_SESSION,''),
+                            'operationCommand'=>"Delete AP Version ".$deleteItem['sfver'],
+                            'operationResult'=>'ok',
+                            'description'=>""
+                        );
+                        Log_Record($this->db,$logary);
+                    }
+                    if ($del_result_array['state']['code']==2000){
+                        $file=$item['uploadPath'];
+                        $delete_file=@unlink ($file);
+                        if($delete_file == true){
+                            $result=$del_result;
+                        }else{
+                            $state=array(
+                                'code'=>6000,
+                                'msg'=>'fail to delete the file in the folder!'
+                            );
+                            $result=$state;
+                        }
+                    } else {
+                        $result=$del_result;
+                    }
                 }
             }
-            else{
-                $result=$del_result;
-                }
-            }
-          }
-      }
-    return $result;
-  }
-	public function index(){
-		$result = null;
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$data = json_decode(file_get_contents("php://input"), true);
-			$result = $this->onAction($data);
-      echo $result;
-		}
-		else if($_SERVER['REQUEST_METHOD'] == 'GET') {
-			$result = $this->fetch();
-      echo json_encode($result);
-		}
-	}
+        }
+        return $result;
+    }
+    public function index(){
+        $result = null;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $this->onAction($data);
+            echo $result;
+        }
+        else if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $result = $this->fetch();
+            echo json_encode($result);
+        }
+    }
 }
--- php/application/controllers/SystemModel.php
+++ php/application/controllers/SystemModel.php
@@ -1,20 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-// require_once('/libraries/Response.php');
 class SystemModel extends CI_Controller {
-	public function __construct() {
-		parent::__construct();
-		$this->load->database();
-		$this->load->helper(array('array', 'my_customfun_helper'));
-		$this->load->library('SqlPage');
-	}
-	function fetch(){
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->database();
+        $this->load->helper(array('array', 'my_customfun_helper'));
+        $this->load->library('SqlPage');
+    }
+    function fetch(){
         $sqlpage = new SqlPage();
-		$columns = 'id,name,radio_num as radionum,vender';
-		$tablenames = 'ap_model';
-		$pageindex = (int)element('page', $_GET, 1);
-		$pagesize = (int)element('size', $_GET, 20);
-		$datalist = $sqlpage->sql_data_page($columns,$tablenames,$pageindex,$pagesize);
+        $columns = 'id,name,radio_num as radionum,vender';
+        $tablenames = 'ap_model';
+        $pageindex = (int)element('page', $_GET, 1);
+        $pagesize = (int)element('size', $_GET, 20);
+        $datalist = $sqlpage->sql_data_page($columns,$tablenames,$pageindex,$pagesize);
         $page=array(
             "start"=>(int) element('start',$_GET,1),
             "size"=> (int)element('size',$_GET,20),
@@ -25,13 +25,13 @@ class SystemModel extends CI_Controller {
             "nextPage"=>(int)element('nextPage',$_GET,-1),
             "lastPage"=>(int)element('lastPage',$_GET,1)
         );
-		$result['state'] = array('code' => 2000, 'msg' => 'ok');
-		$result['data'] = array(
+        $result['state'] = array('code' => 2000, 'msg' => 'ok');
+        $result['data'] = array(
             'page' => $page,
             'list' => $datalist['data']
         );
-		return $result;
-	}
+        return $result;
+    }
     
     function onAction($data) {
         $result = null;
@@ -40,13 +40,49 @@ class SystemModel extends CI_Controller {
         if ($actionType === 'add') {
             $temp_data = $this->getCgiParam($data);
             $result = axc_add_apmodel(json_encode($temp_data));
+            //log
+            $cgiObj = json_decode($result);			
+            if( is_object($cgiObj) && $cgiObj->state->code === 2000) {     
+                $logary = array(
+                    'type'=>'Add',
+                    'operator'=>element('username',$_SESSION,''),
+                    'operationCommand'=>"Add ApModel ".$temp_data['name'],
+                    'operationResult'=>'ok',
+                    'description'=>""
+                );
+                Log_Record($this->db,$logary);
+            }
         } elseif ($actionType === 'edit') {
             $temp_data = $this->getCgiParam($data);
             $result = axc_modify_apmodel(json_encode($temp_data));
+            //log
+            $cgiObj = json_decode($result);			
+            if( is_object($cgiObj) && $cgiObj->state->code === 2000) {     
+                $logary = array(
+                    'type'=>'Update',
+                    'operator'=>element('username',$_SESSION,''),
+                    'operationCommand'=>"Update ApModel ".$temp_data['name'],
+                    'operationResult'=>'ok',
+                    'description'=>json_encode($temp_data)
+                );
+                Log_Record($this->db,$logary);
+            }
         } elseif ($actionType === 'delete') {
             foreach ($selectList as $item) {
                 $deleteItem = array('name' => element('name', $item));
                 $result = axc_del_apmodel(json_encode($deleteItem));
+                //log
+                $cgiObj = json_decode($result);			
+                if( is_object($cgiObj) && $cgiObj->state->code === 2000) {     
+                    $logary = array(
+                        'type'=>'Delete',
+                        'operator'=>element('username',$_SESSION,''),
+                        'operationCommand'=>"Delete ApModel ".$deleteItem['name'],
+                        'operationResult'=>'ok',
+                        'description'=>""
+                    );
+                    Log_Record($this->db,$logary);
+                }
             }
         }
         return $result;
