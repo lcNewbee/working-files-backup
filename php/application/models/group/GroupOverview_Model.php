bb05c5b11446f084272e2205d0a29ab27ce38ed3
yang-xing-hua <yangxinghua_net@163.com>
2017-01-09 19:34:48 +0800

    AXC 更新用户信息，流量统计

--- php/application/models/group/GroupOverview_Model.php
+++ php/application/models/group/GroupOverview_Model.php
@@ -12,7 +12,7 @@ class GroupOverview_Model extends CI_Model {
 	}
 
 	public function get_all($reqdata) {
-		$staticJsonStr = '{"state":{"code":2000,"msg":"ok"},"data":{"cpuUsed":20,"cpuTotal":100,"memoryUsed":40,"memoryTotal":100,"clientsNumber":693,"terminalType":[{"name":"iPhone","value":286},{"name":"Mac","value":164},{"name":"Google","value":126},{"name":"Huaiwei","value":117}],"flowList":[{"name":"http","value":232},{"name":"cifs","value":222},{"name":"smtp","value":180},{"name":"ftp","value":120},{"name":"ssl","value":80},{"name":"https","value":20}],"safeAlarmEvents":[{"name":"DDOS","value":232},{"name":"DNS Cache Snoop","value":345},{"name":"DNS Redirect","value":555},{"name":"DNS Hijacking","value":888}]}}';
+		$staticJsonStr = '{"state":{"code":2000,"msg":"ok"},"data":{"online":0,"offline":0,"clientsNumber":693,"terminalType":[{"name":"iPhone","value":286},{"name":"Mac","value":164},{"name":"Google","value":126},{"name":"Huaiwei","value":117}],"flowList":[{"name":"http","value":232},{"name":"cifs","value":222},{"name":"smtp","value":180},{"name":"ftp","value":120},{"name":"ssl","value":80},{"name":"https","value":20}],"safeAlarmEvents":[{"name":"DDOS","value":232},{"name":"DNS Cache Snoop","value":345},{"name":"DNS Redirect","value":555},{"name":"DNS Hijacking","value":888}]}}';
 
 		$reqdata['groupid'] = (int)$reqdata['groupid'];
 		$reqdata['page'] = (int)$reqdata['page'];
@@ -22,8 +22,24 @@ class GroupOverview_Model extends CI_Model {
 
         $result->data->neighborsAps = $this->get_ap_info($reqdata['groupid'],'getdoubtfulssid');
         $result->data->aroundAps = $this->get_ap_info($reqdata['groupid'],'getaroundssid');
-        //$result->data->flowList = $this->flow_list($reqdata);
-
+        $result->data->flowList = $this->flow_list($reqdata);
+        $retobj = $this->get_overvies($reqdata['groupid']);        
+        $result->data->clientsNumber = $retobj->data->clientnum;//总用户
+        $result->data->online = $retobj->data->apstatus->online;
+        $result->data->offline = $retobj->data->apstatus->offline;
+        
+        $arr = array();
+        if(is_object($retobj->data->clienttype)) {
+            foreach($retobj->data->clienttype as $key=>$val) {
+                $mary['name'] = $key;
+                $mary['value'] = $val;
+                if($mary['name'] === 'Apple'){
+                    $mary['name'] = 'iPhone';
+                }
+                $arr[] = $mary;
+            }
+        }
+        $result->data->terminalType = $arr;//链接数量
 		return $result;
 	}
     //周围AP
@@ -74,8 +90,41 @@ class GroupOverview_Model extends CI_Model {
         $apAry = array();
         $wireessAry = array();
         $clientAry = array();
+        //添加默认值针对小时
+        if($tablename === 'data_flow_hour'){
+            $addnumber = 0;
+            if(count($querydata->result_array()) >0){
+                $st = $querydata->result_array()[0]['Timer'];   
+                $addnumber = (int)substr($st,11,2);//截取时间获取整点                 
+            }else{
+               $addnumber = 24; 
+            }
+            if($addnumber > 0) {
+                for($i = 1; $i <= $addnumber; $i++) {
+                    array_push($apAry,0);           
+                    array_push($wireessAry,0);
+                    array_push($clientAry,0);
+                }       
+            }
+        }
+        //添加默认值针对天数
+        if($tablename === 'data_flow_day'){
+            $default_day = 7;
+            switch($timeType){                
+                case 'half_month': $default_day = 15;
+                    break;
+                case 'month': $default_day = 30;
+                    break;
+            }
+            $add_day = $default_day - count($querydata->result_array());
+            for($j = 1; $j <= $add_day; $j++) {
+                array_push($apAry,0);           
+                array_push($wireessAry,0);
+                array_push($clientAry,0);
+            }                        
+        }
         foreach($querydata->result_array() as $row) {
-            array_push($apAry,$row['ApRxFlow']);
+            array_push($apAry,$row['ApRxFlow']);           
             array_push($wireessAry,$row['RadioRxFlow']);
             array_push($clientAry,$row['StaRxFlow']);
         }
@@ -83,7 +132,7 @@ class GroupOverview_Model extends CI_Model {
             array('name'=>'ap','data'=>$apAry),
             array('name'=>'wireless','data'=>$wireessAry),
             array('name'=>'clients','data'=>$clientAry)
-        );
+        );      
         return $arr;
     }
     public function get_start_end_time($gettype='today') {
@@ -112,5 +161,12 @@ class GroupOverview_Model extends CI_Model {
         $arr['start_date'] = $stateDate;
         $arr['end_date'] = $endDate;
         return $arr;
+    }
+
+    function get_overvies($groupid) {
+        $cgiarr = array('groupid'=>$groupid);
+        $cgistr = axc_get_overvies(json_encode($cgiarr));
+        $retobj = json_decode($cgistr);
+        return $retobj;
     }
 }
