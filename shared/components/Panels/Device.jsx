82e921c783b93ad22af7787c085ffc89c987ba08
xiechengyu <taijisanfeng@yahoo.com>
2017-03-04 10:29:28 +0800

    AXC: 隐藏未实现功能

--- shared/components/Panels/Device.jsx
+++ shared/components/Panels/Device.jsx
@@ -143,8 +143,8 @@ function DevicesProperties(props) {
                         }
                       }
 
-                      // 小于 2.5 版本
-                      if (versionCode < 20500) {
+                      // 小于 2.5 版本， 功能未实现暂时隐藏
+                      if (versionCode < 40500) {
                         if (panelKey === 'radioQos') {
                           return null;
                         }
