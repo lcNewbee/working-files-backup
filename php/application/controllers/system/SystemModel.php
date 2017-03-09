a0f09a96c75421593e3f44729aeb0c8debd1f72f
yang-xing-hua <yangxinghua_net@163.com>
2017-03-09 17:49:36 +0800

    AXC 提交AP型号页面分页问题

--- php/application/controllers/system/SystemModel.php
+++ php/application/controllers/system/SystemModel.php
@@ -6,28 +6,16 @@ class SystemModel extends CI_Controller {
         $this->load->library('session');
         $this->load->database();
         $this->load->helper(array('array', 'my_customfun_helper'));
-        $this->load->library('SqlPage');
     }
     function fetch(){
-        $sqlpage = new SqlPage();
         $columns = 'id,name,radio_num as radionum,vender';
         $tablenames = 'ap_model';
         $pageindex = (int)element('page', $_GET, 1);
         $pagesize = (int)element('size', $_GET, 20);
-        $datalist = $sqlpage->sql_data_page($columns,$tablenames,$pageindex,$pagesize);
-        $page=array(
-            "start"=>(int) element('start',$_GET,1),
-            "size"=> (int)element('size',$_GET,20),
-            "currPage"=>(int)element('currPage',$_GET,1),
-            "totalPage"=>(int)element('totalPage',$_GET,1),
-            "total"=>(int)element('total_row',$datalist,20),
-            "pageCount"=>(int)element('total_page',$datalist,11),
-            "nextPage"=>(int)element('nextPage',$_GET,-1),
-            "lastPage"=>(int)element('lastPage',$_GET,1)
-        );
+        $datalist = help_data_page($this->db,$columns,$tablenames,$pageindex,$pagesize);  
         $result['state'] = array('code' => 2000, 'msg' => 'ok');
         $result['data'] = array(
-            'page' => $page,
+            'page' => $datalist['page'],
             'list' => $datalist['data']
         );
         return $result;
