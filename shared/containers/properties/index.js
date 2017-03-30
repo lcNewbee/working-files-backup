c7fb0817b4203956cc88b3be9ca2fe32abab6e92
LCL <chenlin@axilspot.com>
2017-03-30 10:56:51 +0800

    Platform: 先彻底删除文件名冲突的文件夹

--- shared/containers/properties/index.js
+++
@@ -1,9 +0,0 @@
-import * as actions from './actions';
-import reducer from './reducer';
-import Properties from './components/Properties';
-
-export default {
-  actions,
-  reducer,
-  Screen: Properties,
-};
