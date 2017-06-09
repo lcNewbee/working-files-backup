f8d2c736e4d80ebd84cc4c087dfbe5c0fba1ca8c
Gruis.w <gruis.w@gmail.com>
2017-06-09 11:59:40 +0800

    Platform: 优化 gulp pub 命令

--- php/application/models/system/Login_Model.php
+++ php/application/models/system/Login_Model.php
@@ -1,6 +1,6 @@
 <?php
 class Login_Model extends CI_Model {
-	public function __construct() {		
+	public function __construct() {
 		parent::__construct();
 		$this->load->library('session');
 		$this->load->database();
@@ -18,12 +18,12 @@ class Login_Model extends CI_Model {
 			$result['data'] = array(
 				'purview'=>$datarow->purview,
 				'usertype'=>$datarow->usertype
-			);	
+			);
 			$sessiondata = array(
 				'username' => $username,
 				'purview' => $datarow->purview
 			);
-			$this->session->set_userdata($sessiondata);			
+			$this->session->set_userdata($sessiondata);
 			$logary = array(
 				'type'=>'login',
 				'operator'=>$username,
--- tools/gulp/pub.js
+++ tools/gulp/pub.js
@@ -5,6 +5,7 @@ const runSequence = require('run-sequence');
 const $ = require('gulp-load-plugins')();
 const argv = require('minimist')(process.argv.slice(2));
 const zip = require('gulp-zip');
+const getCurAppName = require('../shared/getCurAppName');
 
 const paths = gulp.paths;
 
@@ -26,8 +27,12 @@ gulp.task('pub:build', (callback) => {
   // 产品系列：axc（默认值）, ap, ac
   let productSeries = 'axc';
 
-  if (argv.n && argv.n.indexOf('axc') !== 0) {
-    productSeries = argv.n;
+  if (!gulp.appName) {
+    gulp.appName = getCurAppName('axc');
+  }
+
+  if (gulp.appName && gulp.appName.indexOf('axc') !== 0) {
+    productSeries = gulp.appName;
   }
 
   // 只有在 AXC 系列才需要做，goform 路径相关替换
@@ -41,8 +46,18 @@ gulp.task('pub:build', (callback) => {
 });
 
 gulp.task('pub:copy', () => {
+  const srcPaths = [`${paths.build}/**/*`];
   let distPath = paths.pub;
   let name = 'axc';
+
+  if (!gulp.appName) {
+    gulp.appName = getCurAppName('axc');
+  }
+
+  // 如果是 AXC 系列, 需要拷贝 PHP
+  if (gulp.appName && gulp.appName.indexOf('axc') === 0) {
+    srcPaths.push(`${paths.php}/**/*`);
+  }
 
   if (argv.d) {
     distPath = argv.d;
@@ -53,13 +68,13 @@ gulp.task('pub:copy', () => {
   }
 
   if (argv.z) {
-    return gulp.src([`${paths.build}/**/*`, `${paths.php}/**/*`])
+    return gulp.src(srcPaths)
       .pipe(gulp.dest(distPath))
       .pipe(zip(`${name}.zip`))
       .pipe(gulp.dest(paths.tmp));
   }
 
-  return gulp.src([`${paths.build}/**/*`, `${paths.php}/**/*`])
+  return gulp.src(srcPaths)
     .pipe(gulp.dest(distPath));
 });
 
