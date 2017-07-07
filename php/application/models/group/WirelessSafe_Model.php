aed80bced33cd4838094763cf817967bf03c3e9c
yang-xing-hua <yangxinghua_net@163.com>
2017-07-07 18:17:42 +0800

    AXC 提交 AP监控设置整改

--- php/application/models/group/WirelessSafe_Model.php
+++ php/application/models/group/WirelessSafe_Model.php
@@ -6,26 +6,61 @@ class WirelessSafe_Model extends CI_Model {
 		$this->load->database();
 		$this->load->helper(array('array', 'my_customfun_helper'));
 	}
-	public function get_wips_list($retdata) {
-        $arr['state'] = array("code"=>2000,"msg"=>"ok");
+	public function get_wips_list($retdata) {              
         $queryd = $this->db->select('enable,cycles,scantype,apopermode,rpttime,chlnum,enable2g4chl,enable2g4pwr,adjafactor2g4,enable5gchl,enable5gpwr,adjafactor5g,maxtxpwr')
                             ->from('wrrm_template')
                             ->where('id', $retdata['groupid'])
                             ->get()
                             ->result_array();
+        $retary = array();                    
         if(count($queryd) > 0) {
-            $arr['data'] = array("settings"=>$queryd[0]);
-        }else{
-            $arr['state'] = array("code"=>4000,"msg"=>"not");
+            $retary = $queryd[0];   
+            //页面UI需要的是字段是scanSpectrum 需要转换，值也要做转换 
+            $scanSpectrum = '';
+            $ary = explode(",", $queryd[0]['chlnum']);
+            if(in_array('1', $ary)){
+                $scanSpectrum = '2';
+            }
+            if(in_array('36', $ary)){
+                $scanSpectrum = '5';
+            }
+            if(in_array('1', $ary) && in_array('36', $ary)){
+                $scanSpectrum = '2,5';
+            }
+            $retary['scanSpectrum'] = $scanSpectrum;
         }
+
+        $arr = array(
+            'state' => array('code' => 2000, 'msg' => 'ok'),
+            'data' => array(
+                'settings' => $retary
+            )
+        );
+
         return json_encode($arr);
     }
 
     public function add_wips_cfg($data) {
         $result = null;
-        $arr = $data;
-        $arr['groupid'] = (int)element('groupid',$data);
-
+        $arr = array();
+        foreach($data as $key => $value){
+            if($key == 'action'){
+                continue;
+            }
+            if($key == 'scanSpectrum'){
+                if($value == '2'){
+                    $arr['chltype'] = 1;
+                }
+                if($value == '5'){
+                    $arr['chltype'] = 2;
+                }
+                if($value == '2,5' || $value == '5,2'){
+                    $arr['chltype'] = 3;
+                }
+                continue;
+            }
+            $arr[$key] = $value;
+        }
         $result = wrrm_set_param(json_encode($arr));
         //log
         $cgiObj = json_decode($result);
@@ -39,7 +74,7 @@ class WirelessSafe_Model extends CI_Model {
             );
             Log_Record($this->db,$logary);
         }
-        return $result;
+        return $result;        
     }
 
 }
