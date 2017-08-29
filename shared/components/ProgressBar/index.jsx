ead44833df3cf5b67d40445e1b7b4db16e6b5e46
lichao <charles.li@axilspot.com>
2017-08-29 17:03:22 +0800

    用requestAnimationFrame重写ProgressBar组件

--- shared/components/ProgressBar/index.jsx
+++ shared/components/ProgressBar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Progress from '../Progress';
+// import Progress from '../Progress';
 
 window.requestAnimationFrame = window.requestAnimationFrame
                             || window.mozRequestAnimationFrame
@@ -13,18 +13,20 @@ window.cancelAnimationFrame = window.cancelAnimationFrame
                             || function (requestId) { clearTimeout(requestId); };
 const propTypes = {
   title: PropTypes.string, // 进度条上方显示的提示文字
-
-  isShow: PropTypes.bool, // 控制组件是否显示
   start: PropTypes.bool, // start属性控制进度条是否开始显示进度，为true表示开始移动，默认为false
-  style: PropTypes.object, // 设置进度条的样式
   callback: PropTypes.func, // 进度条走完后执行的函数
+  showText: PropTypes.bool, // 是否显示进度条文字，默认true
+  unit: PropTypes.string, // 进度条单位，默认%
+  color: PropTypes.string, // 进度条颜色
 
   // 进度条总时长,单位为 秒（s）
   time: PropTypes.number,
 };
 
 const defaultProps = {
-  isShow: false,
+  showText: true,
+  color: '#0069d9',
+  unit: '%',
   start: false,
   time: 30, // 默认总时间为 30 s
 };
@@ -41,7 +43,6 @@ export default class ProgressBar extends React.Component {
 
   componentDidMount() {
     if (this.props.start === true) {
-      console.log('is in ms brower', window.requestAnimationFrame === window.msRequestAnimationFrame);
       requestAnimationFrame(this.oneStepMove);
     }
   }
@@ -57,29 +58,24 @@ export default class ProgressBar extends React.Component {
   }
 
   oneStepMove(timestamp) {
-    console.log('timestamp', timestamp);
     const timeStamp = timestamp || new Date().getTime();
     if (!this.startTime) { this.startTime = timeStamp; }
     const runtime = timeStamp - this.startTime;
     let progress = runtime / (this.props.time * 1000);
     progress = Math.min(progress, 1);
-    this.setState({ progress }, () => {
-      if (runtime < this.props.time * 1000) {
-        this.requestId = requestAnimationFrame(this.oneStepMove);
-      } else {
-        cancelAnimationFrame(this.requestId);
-        this.props.callback();
-      }
-    });
+    this.setState({ progress });
+    if (runtime < this.props.time * 1000) {
+      this.requestId = requestAnimationFrame(this.oneStepMove);
+    } else {
+      cancelAnimationFrame(this.requestId);
+      this.props.callback();
+    }
   }
 
   render() {
-    const { title, ...restProps } = this.props;
+    const { title, showText, unit, color } = this.props;
     const percentageValue = parseFloat(this.state.progress * 100);
-
-    delete restProps.time;
-    delete restProps.isShow;
-    delete restProps.callback;
+    const percentageText = parseInt(percentageValue, 10);
 
     return (
       <div className="m-progress-bar">
@@ -90,14 +86,28 @@ export default class ProgressBar extends React.Component {
             </div>
           ) : null
         }
-        <Progress
-          {...restProps}
-          className="m-progress-bar__body"
-          value={percentageValue > 100 ? 100 : percentageValue}
-          max="100"
-          unit="% ..."
-          showText
-        />
+        <div
+          className="a-progress-wrap"
+          style={{
+            borderRadius: '.25rem',
+            overflow: 'hidden',
+            marginBottom: '5px',
+            backgroundColor: '#e9ecef',
+          }}
+        >
+          <div
+            style={{
+              width: `${percentageValue}%`,
+              backgroundColor: color,
+              height: '16px',
+            }}
+          />
+          {
+            showText && (
+              <span className="a-progress-text">{`${percentageText}${unit}`}</span>
+            )
+          }
+        </div>
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ const redux = require('redux');
 const thunkMiddleware = require('redux-thunk').default;
 const Provider = require('react-redux').Provider;
 const AppContainer = require('react-hot-loader').AppContainer;
-const prodConfig = require('./config/NHZYASW120').default;
+const prodConfig = require('./config/AEC120').default;
 
 const combineReducers = redux.combineReducers;
 const applyMiddleware = redux.applyMiddleware;
@@ -66,9 +66,9 @@ window.onload = () => {
 
 // Enable hot reload by react-hot-loader
 if (module.hot) {
-  module.hot.accept('./config/NHZYASW120', () => {
+  module.hot.accept('./config/AEC120', () => {
     /* eslint-disable global-require */
-    const nextConfig = require('./config/NHZYASW120').default;
+    const nextConfig = require('./config/AEC120').default;
 
     stores.replaceReducer(combineReducers({
       ...nextConfig.reducers,
