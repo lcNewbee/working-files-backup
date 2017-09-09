1c1a36721b1c3626752ca0780fe0c2b9ff35075e
Gruis.w <gruis.w@gmail.com>
2017-09-09 12:26:08 +0800

    Shared: 先回退 有关 ProgressBar 组件修改

--- shared/components/ProgressBar/index.jsx
+++ shared/components/ProgressBar/index.jsx
@@ -1,74 +1,109 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Progress from '../Progress';
+import Progress from '../Progress';
 
-window.requestAnimationFrame = window.requestAnimationFrame
-                            || window.mozRequestAnimationFrame
-                            || window.webkitRequestAnimationFrame
-                            || window.msRequestAnimationFrame
-                            || function (f) { return setTimeout(f, 1000 / 60); };
-
-window.cancelAnimationFrame = window.cancelAnimationFrame
-                            || window.mozCancelAnimationFrame
-                            || function (requestId) { clearTimeout(requestId); };
 const propTypes = {
-  title: PropTypes.string, // 进度条上方显示的提示文字
-  start: PropTypes.bool, // start属性控制进度条是否开始显示进度，为true表示开始移动，默认为false
-  callback: PropTypes.func, // 进度条走完后执行的函数
-  showText: PropTypes.bool, // 是否显示进度条文字，默认true
-  unit: PropTypes.string, // 进度条单位，默认%
-  color: PropTypes.string, // 进度条颜色
-  time: PropTypes.number, // 进度条总时长,单位为 秒（s）
+  title: PropTypes.string,    // 进度条上方显示的提示文字
+
+  isShow: PropTypes.bool,     // 控制组件是否显示
+  start: PropTypes.bool,      // start属性控制进度条是否开始显示进度，为true表示开始移动，默认为false
+  style: PropTypes.object,    // 设置进度条的样式
+  callback: PropTypes.func,   // 进度条走完后执行的函数
+
+  // 进度条总时长,单位为 秒（s）
+  time: PropTypes.number,
+
+  // 每 1% 间隔时间,单位为 毫秒（ms）
+  initStep: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  curStep: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  step: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 const defaultProps = {
-  showText: true,
-  color: '#0069d9',
-  unit: '%',
+  isShow: false,
   start: false,
-  time: 30, // 默认总时间为 30 s
+  initStep: 0,
+  time: 30,    // 默认总时间为 30 s
 };
 
 export default class ProgressBar extends React.Component {
   constructor(props) {
     super(props);
-    this.oneStepMove = this.oneStepMove.bind(this);
+    this.startMove = this.startMove.bind(this);
+    this.state = {
+      width: 0,
+      n: props.initStep,
+    };
   }
 
   componentDidMount() {
     if (this.props.start === true) {
-      requestAnimationFrame(this.oneStepMove);
+      this.startMove();
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.initStep !== this.props.initStep) {
+      this.setState({
+        n: nextProps.initStep,
+      });
+    }
+
+    if (nextProps.curStep !== this.props.curStep) {
+      this.setState({
+        n: nextProps.curStep,
+      });
     }
   }
 
   componentDidUpdate(prevProps) {
     if ((prevProps.start === false && this.props.start === true) ||
-        prevProps.time !== this.props.time) {
-      requestAnimationFrame(this.oneStepMove);
+        prevProps.step !== this.props.step || prevProps.time !== this.props.time) {
+      this.startMove();
     }
   }
   componentWillUnmount() {
-    cancelAnimationFrame(this.requestId);
+    clearInterval(this.barChangeInterval);
   }
 
-  oneStepMove(timestamp) {
-    const timeStamp = timestamp || new Date().getTime();
-    if (!this.startTime) { this.startTime = timeStamp; }
-    const runtime = timeStamp - this.startTime;
-    let progress = runtime / (this.props.time * 1000);
-    progress = Math.min(progress, 1);
-    this.pgsElem.style.width = `${progress * 100}%`;
-    this.txtElem.textContent = `${parseInt(progress * 100, 10)}${this.props.unit}`;
-    if (runtime < this.props.time * 1000) {
-      this.requestId = requestAnimationFrame(this.oneStepMove);
+  startMove() {
+    const time = this.props.time || 30;
+    let intervalTime = (time * 1000) / 100;
+
+    if (this.props.step) {
+      intervalTime = parseInt(this.props.step, 10);
     } else {
-      cancelAnimationFrame(this.requestId);
-      this.props.callback();
+      intervalTime = (time * 1000) / 100;
     }
+
+    clearInterval(this.barChangeInterval);
+
+    this.barChangeInterval = setInterval(() => {
+      const n = this.state.n;
+
+      if (n <= 100) {
+        this.setState({
+          n: n + 1,
+        });
+
+      // 到达 100 清理定时器，执行回调
+      } else {
+        clearInterval(this.barChangeInterval);
+        if (this.props.callback && typeof (this.props.callback) === 'function') {
+          this.props.callback();
+        }
+      }
+    }, intervalTime);
   }
 
   render() {
-    const { title, showText, unit, color } = this.props;
+    const { title, ...restProps } = this.props;
+    const percentageValue = parseInt(this.state.n, 10);
+
+    delete restProps.initStep;
+    delete restProps.time;
+    delete restProps.curStep;
+    delete restProps.isShow;
+    delete restProps.callback;
 
     return (
       <div className="m-progress-bar">
@@ -79,42 +114,14 @@ export default class ProgressBar extends React.Component {
             </div>
           ) : null
         }
-        <div
-          className="a-progress-wrap"
-          style={{
-            borderRadius: '.25rem',
-            overflow: 'hidden',
-            marginBottom: '5px',
-            backgroundColor: '#e9ecef',
-          }}
-        >
-          <div
-            style={{
-              width: 0,
-              backgroundColor: color,
-              height: '16px',
-            }}
-            ref={(pgsElem) => {
-              if (pgsElem) {
-                this.pgsElem = pgsElem;
-              }
-            }}
-          />
-          {
-            showText && (
-              <span
-                className="a-progress-text"
-                ref={(txtElem) => {
-                  if (txtElem) {
-                    this.txtElem = txtElem;
-                  }
-                }}
-              >
-                {`0${unit}`}
-              </span>
-            )
-          }
-        </div>
+        <Progress
+          {...restProps}
+          className="m-progress-bar__body"
+          value={percentageValue > 100 ? 100 : percentageValue}
+          max="100"
+          unit="% ..."
+          showText
+        />
       </div>
     );
   }
